478319b14d0bfa9620e6494b0b47b313c3b161a5
Diego Samuel Alves <diego.samuel.alves@gmail.com>
2018-08-22 15:03:53 -0300

    Criando singleton conversao

--- cadastro-service/src/main/kotlin/br/com/projetoaplicado/cadastro/util/ModelMapperConverter.kt
+++ cadastro-service/src/main/kotlin/br/com/projetoaplicado/cadastro/util/ModelMapperConverter.kt
@@ -4,12 +4,17 @@ import org.modelmapper.ModelMapper
 import org.modelmapper.convention.MatchingStrategies
 
 class ModelMapperConverter {
+    
+    private val modelMapper: ModelMapper = ModelMapper()
+    private object Holder { val INSTANCE = ModelMapperConverter() }
+    
+    companion object {
+        val instance: ModelMapperConverter by lazy { Holder.INSTANCE }
+    }
 
-    var modelMapper: ModelMapper = ModelMapper()
-
-    fun converter(source: Any, target: Class<out Any>): Any {
+    public fun converter(source: Any, target: Class<out Any>): Any {
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT)
         return modelMapper.map(source, target)
     }
-
+    
 }
