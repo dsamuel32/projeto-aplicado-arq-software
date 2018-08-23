5927807dd111b6c659ea06c97221221445739969
dsamuel32 <diego.samuel.alves@gmail.com>
2018-08-22 21:04:51 -0300

    Criando modulo comum

--- cadastro-service/src/main/kotlin/br/com/projetoaplicado/cadastro/service/impl/AlunoServiceImpl.kt
+++ cadastro-service/src/main/kotlin/br/com/projetoaplicado/cadastro/service/impl/AlunoServiceImpl.kt
@@ -3,7 +3,7 @@ package br.com.projetoaplicado.cadastro.service.impl
 import br.com.projetoaplicado.cadastro.dominio.Pessoa
 import br.com.projetoaplicado.cadastro.dominio.PessoaDTO
 import br.com.projetoaplicado.cadastro.service.AlunoService
-import br.com.projetoaplicado.cadastro.util.ModelMapperConverter
+import br.com.projetoaplicado.comum.util.ModelMapperConverter
 import org.springframework.stereotype.Service
 
 @Service
--- comum/src/main/kotlin/br/com/projetoaplicado/comum/util/ModelMapperConverter.kt
+++ comum/src/main/kotlin/br/com/projetoaplicado/comum/util/ModelMapperConverter.kt
@@ -1,20 +1,19 @@
-package br.com.projetoaplicado.cadastro.util
+package br.com.projetoaplicado.comum.util
 
 import org.modelmapper.ModelMapper
 import org.modelmapper.convention.MatchingStrategies
 
-class ModelMapperConverter {
-    
-    private val modelMapper: ModelMapper = ModelMapper()
+class ModelMapperConverter(private val modelMapper: ModelMapper = ModelMapper()) {
+
     private object Holder { val INSTANCE = ModelMapperConverter() }
-    
+
     companion object {
         val instance: ModelMapperConverter by lazy { Holder.INSTANCE }
     }
 
-    public fun converter(source: Any, target: Class<out Any>): Any {
+    fun converter(source: Any, target: Class<out Any>): Any {
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT)
         return modelMapper.map(source, target)
     }
-    
+
 }
