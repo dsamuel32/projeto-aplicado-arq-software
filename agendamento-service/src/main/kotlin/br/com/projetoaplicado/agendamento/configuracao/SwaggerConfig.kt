dac52cfea63768725ff5dc097bd5ba5864a450fa
dsamuel32 <diego.samuel.alves@gmail.com>
2018-08-27 22:20:12 -0300

    Adiconando configuracoes swagger

---
+++ agendamento-service/src/main/kotlin/br/com/projetoaplicado/agendamento/configuracao/SwaggerConfig.kt
@@ -0,0 +1,36 @@
+package br.com.projetoaplicado.agendamento.configuracao
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiInfo
+import springfox.documentation.service.Contact
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+import java.util.*
+
+@Configuration
+@EnableSwagger2
+class SwaggerConfig {
+
+    @Bean
+    fun api(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("br.com.projetoaplicado.agendamento.apresentacao"))
+                .paths(PathSelectors.ant("/**"))
+                .build()
+                .apiInfo(apiInfo())
+    }
+
+    fun apiInfo(): ApiInfo {
+        return ApiInfo(
+                "CINEMA REST API",
+           "CINEMA API.",
+              "0.0.1", "Termos de serviço",
+                      Contact("Dev", "br.com.netflixossplaygrond.cinema", "myeaddress@company.com"),
+        "License of API", "API license URL", Collections.emptyList())
+    }
+}
